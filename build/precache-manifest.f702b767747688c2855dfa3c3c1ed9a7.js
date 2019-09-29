9d65328ef129bb5186bdd7ae9ddc759f20520760
Sahil Kapur <sahilkapur.a@gmail.com>
2019-09-29 16:43:53 -0400

    Add build version

---
+++ build/precache-manifest.f702b767747688c2855dfa3c3c1ed9a7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc70a89478fe7fdf94a406a153d7bd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f493ab8852d07eadc6d6",
+    "url": "/static/js/2.1d1c1f80.chunk.js"
+  },
+  {
+    "revision": "65745418ee98b7eb8291",
+    "url": "/static/js/main.7fad7a5c.chunk.js"
+  },
+  {
+    "revision": "f676bd830ab6665ac0f6",
+    "url": "/static/js/runtime-main.d725e427.js"
+  }
+]);
